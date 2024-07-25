8093da39110806112945b30236e1d2f55f0e7649
Mehmet Can <m3hm3tcan@gmail.com>
2024-07-25 21:41:18 +0200

    Create parameter-test.py

---
+++ parameter-test.py
@@ -0,0 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser("simple_example")
+parser.add_argument("org")
+parser.add_argument("projectName")
+parser.add_argument("workItemType")
+parser.add_argument("ruleType")
+parser.add_argument(" stageCategory")
+args=parser.parse_args()
+
+print(args.org, ', ',args.projectName,', ',args.workItemType,', ',args.ruleType,', ',args.stageCategory)
