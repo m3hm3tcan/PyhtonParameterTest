7b8eaa9138f375c37531b4fd3deca4acff4ab522
Mehmet Can <m3hm3tcan@gmail.com>
2024-07-25 22:26:11 +0200

    Update parameter-test.py

--- parameter-test.py
+++ parameter-test.py
@@ -1,12 +1,12 @@
 import argparse
 import parser
 
-parser = argparse.ArgumentParser("simple_example")
-parser.add_argument("org")
-parser.add_argument("projectName")
-parser.add_argument("workItemType")
-parser.add_argument("ruleType")
-parser.add_argument(" stageCategory")
+parser = argparse.ArgumentParser()
+parser.add_argument("--org",required=True)
+parser.add_argument("--projectName",required=True)
+parser.add_argument("--workItemType",required=True)
+parser.add_argument("--ruleType",required=True)
+parser.add_argument("--stageCategory",required=True)
 args=parser.parse_args()
 
 print(args.org, ', ',args.projectName,', ',args.workItemType,', ',args.ruleType,', ',args.stageCategory)
